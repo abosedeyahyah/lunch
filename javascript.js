1906212e798dd10f3deeefb6e9fd86f5186be623
abosedeyahyah <108008221+abosedeyahyah@users.noreply.github.com>
2022-10-05 02:17:32 -0700

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,17 @@
+
+var names = ["aminat", "David", "smith", "Ola"];
+
+
+function whosPaying(names){
+
+    var names_length = names.length;
+    var personnames = Math.floor(Math.random() * names_length );
+    var randomperson = names[personnames];
+    var output = randomperson + " is going to pay for the lunch today!";
+
+    return output
+    
+}
+
+whosPaying(names);
+console.log( whosPaying(names) );
